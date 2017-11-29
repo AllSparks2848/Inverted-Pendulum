bb5ba3fdd587f2c3491764f8acc2f9bee62eb2f1
Unknown <18187@jcpstudents.org>
2017-11-29 16:55:09 -0600

    Tuned lightly better

--- poleplace_cartpend.m
+++ poleplace_cartpend.m
@@ -22,9 +22,9 @@ eig(A)
 % p is a vector of desired eigenvalues
 % p = [-.01; -.02; -.03; -.04]; % not enough
 %p = [-.3; -.4; -.5; -.6];  % just barely
-%p = [-1; -1.1; -1.2; -1.3]; % good
+p = [-1; -1.1; -1.2; -1.3]; % good
 %p = [-2; -2.1; -2.2; -2.3]; % aggressive
-p = [-3; -3.1; -3.2; -3.3]; % aggressive
+%p = [-3; -3.1; -3.2; -3.3]; % aggressive
 % p = [-3.5; -3.6; -3.7; -3.8]; % breaks
 K = place(A,B,p);
 % K = lqr(A,B,Q,R);
@@ -37,7 +37,7 @@ elseif(s==1)
     y0 = [-3; 0; pi+.1; 0];
 %     [t,y] = ode45(@(t,y)cartpend(y,m,M,L,g,d,-K*(y-[1; 0; pi; 0])),tspan,y0);
     [t,y] = ode45(@(t,y)cartpend(y,m,M,L,g,d,-K*(y-[1; 0; pi; 0])),tspan,y0);
-else 
+else
 end
 
 for k=1:100:length(t)
